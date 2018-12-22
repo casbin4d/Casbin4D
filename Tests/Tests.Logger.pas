f647185f7372bccbd3bdda6ff293a3fc2e1469e3
John Kouraklis <j_kour@hotmail.com>
2018-12-22 19:49:53 +0000

    + Tests.Logger

---
+++ Tests/Tests.Logger.pas
@@ -0,0 +1,69 @@
+unit Tests.Logger;
+
+interface
+
+uses
+  DUnitX.TestFramework, Core.Logger.Types;
+
+type
+  [TestFixture]
+  TTestLogger = class(TObject)
+  private
+    fLogger: ILogger;
+  public
+    [Setup]
+    procedure Setup;
+    [TearDown]
+    procedure TearDown;
+
+    [Test]
+    procedure testEnabled;
+
+    [Test]
+    procedure testLogWhenEnabled;
+
+    [Test]
+    procedure testLogWhenDisabled;
+  end;
+
+implementation
+
+uses
+  Core.Logger.Default;
+
+procedure TTestLogger.Setup;
+begin
+  fLogger:=TDefaultLogger.Create;
+end;
+
+procedure TTestLogger.TearDown;
+begin
+  fLogger:=nil;
+end;
+
+procedure TTestLogger.testEnabled;
+begin
+  Assert.IsTrue(fLogger.Enabled);
+  fLogger.Enabled:=False;
+  Assert.IsFalse(fLogger.Enabled);
+  fLogger.Enabled:=True;
+  Assert.IsTrue(fLogger.Enabled);
+end;
+
+procedure TTestLogger.testLogWhenDisabled;
+begin
+  fLogger.Enabled:=False;
+  fLogger.log('Disabled');
+  Assert.IsTrue(fLogger.LastLoggedMessage='');
+end;
+
+procedure TTestLogger.testLogWhenEnabled;
+begin
+  fLogger.Enabled:=True;
+  fLogger.log('Test');
+  Assert.AreEqual('Test', fLogger.LastLoggedMessage);
+end;
+
+initialization
+  TDUnitX.RegisterTestFixture(TTestLogger);
+end.

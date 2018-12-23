077c9ae060821729d20d86150766ee960f2128df
John Kouraklis <j_kour@hotmail.com>
2018-12-23 04:05:16 +0000

    +Lexer.Tokeniser method

---
+++ SourceCode/Common/Lexer/Lexer.Tokeniser.Types.pas
@@ -0,0 +1,30 @@
+unit Lexer.Tokeniser.Types;
+
+interface
+
+uses
+  Core.Base.Types, Core.Logger.Types, Lexer.Tokens.List,
+  System.Generics.Collections, Lexer.Tokens.Messages, System.SysUtils;
+
+type
+  ELexerException = class (Exception);
+
+  TTokeniserStatus = (tsNotStarted, tsRunning, tsFatalError, tsFinished);
+  ITokeniser = interface (IBaseInterface)
+    ['{77DA3C9D-41ED-4E20-9A1A-9A835538BD1D}']
+    function getLogger: ILogger;
+    function getStatus: TTokeniserStatus;
+    function getTokenList: TTokenList;
+    function getTokenMessages: TObjectList<TTokenMessage>;
+    procedure setLogger(const aValue: ILogger);
+    procedure tokenise;
+
+    property Logger: ILogger read getLogger write setLogger;
+    property Status: TTokeniserStatus read getStatus;
+    property TokenList: TTokenList read getTokenList;
+    property TokenMessages: TObjectList<TTokenMessage> read getTokenMessages;
+  end;
+
+implementation
+
+end.
---
+++ SourceCode/Common/Lexer/Lexer.Tokeniser.pas
@@ -0,0 +1,166 @@
+unit Lexer.Tokeniser;
+
+interface
+
+uses
+  Core.Base.Types, Lexer.Tokens.Messages, Lexer.Tokens.List, Core.Logger.Types,
+  System.Generics.Collections, Lexer.Tokeniser.Types;
+
+type
+  TTokeniser = class (TBaseInterfacedObject, ITokeniser)
+  private
+    fInputString: string;
+    fTokenList: TTokenList;
+    fLogger: ILogger;
+    fTokenMessages: TObjectList<TTokenMessage>;
+    fStatus: TTokeniserStatus;
+  private
+{$REGION 'Interface'}
+    function getLogger: ILogger;
+    function getTokenList: TTokenList;
+    function getTokenMessages: TObjectList<TTokenMessage>;
+    procedure setLogger(const aValue: ILogger);
+    procedure tokenise;
+    function getStatus: TTokeniserStatus;
+{$ENDREGION}
+  public
+    constructor Create(const aInputString: string);
+    destructor Destroy; override;
+  end;
+
+implementation
+
+uses
+  Core.Logger.Default, System.SysUtils, Lexer.Tokens.Types, System.StrUtils, Lexer.Utilities;
+
+constructor TTokeniser.Create(const aInputString: string);
+begin
+  inherited Create;
+  fLogger:=TDefaultLogger.Create;
+  fTokenList:=TTokenList.Create;
+  fTokenMessages:=TObjectList<TTokenMessage>.Create(true);
+  fInputString:=Trim(aInputString);
+  fStatus:=tsNotStarted;
+end;
+
+destructor TTokeniser.Destroy;
+begin
+  fLogger:=nil;
+  fTokenMessages.Free;
+  fTokenList.Free;
+  inherited;
+end;
+
+function TTokeniser.getLogger: ILogger;
+begin
+  Result:=fLogger;
+end;
+
+function TTokeniser.getStatus: TTokeniserStatus;
+begin
+  Result:=fStatus;
+end;
+
+function TTokeniser.getTokenList: TTokenList;
+begin
+  Result:=fTokenList;
+end;
+
+function TTokeniser.getTokenMessages: TObjectList<TTokenMessage>;
+begin
+  Result := fTokenMessages;
+end;
+
+procedure TTokeniser.setLogger(const aValue: ILogger);
+begin
+  fLogger:=nil;
+  fLogger:=aValue;
+end;
+
+procedure TTokeniser.tokenise;
+var
+  tokenMessage: TTokenMessage;
+  currPosition: TPosition;
+  lastPosition: TPosition;
+  currCh: Char;
+  value: string;
+  token: PToken;
+begin
+  fTokenList.Clear;
+  fTokenMessages.Clear;
+
+  currPosition.Column:=Low(string);
+  currPosition.Row:=0;
+  lastPosition.Column:=currPosition.Column;
+  lastPosition.Row:=currPosition.Row;
+  fStatus:=tsRunning;
+
+  // Empty parse string is passed
+  if Trim(fInputString) = '' then
+  begin
+//    tokenMessage.Create(tmtError, 'Empty string to parse', position);
+//    tokenMessage.ErrorType:=tmeSyntaxError;
+//    fTokenMessages.Add(tokenMessage);
+    fStatus:=tsFatalError;
+    Exit;
+  end;
+
+  for currCh in fInputString do
+  begin
+    if CharInSet(currCh, [#13, #10]) then
+    begin
+      Inc(currPosition.Row);
+      currPosition.Column:=Low(string);
+    end
+    else
+    begin
+      if not CharInSet(currCh, [#32, #9]) then
+      begin
+        if not CharInSet(currCh, oneCharReserved) then
+        begin
+          value:=value+currCh;
+          if ((currPosition.Column+1)<=High(fInputString)) and
+            (CharInSet(fInputString[currPosition.Column+1],
+                                          oneCharReserved)) then
+          begin
+            //Here we must check for keywords
+            //For now we declare it as identifier
+            New(token);
+            FillChar(token^, SizeOf(TToken), 0);
+            token^.&Type:=ttIdentifier;
+            token^.Value:=value;
+
+            token.StartPosition.Column:=lastPosition.Column;
+            token.StartPosition.Row:=lastPosition.Row;
+            token.EndPosition.Column:=currPosition.Column;
+            token.EndPosition.Row:=currPosition.Row;
+
+            fTokenList.Add(token);
+
+            lastPosition.Column:=currPosition.Column+1;
+            lastPosition.Row:=currPosition.Row;
+          end;
+        end
+        else
+        begin
+          token:=tokenForOneCharReserved(currCh);
+
+          token.StartPosition.Column:=lastPosition.Column;
+          token.StartPosition.Row:=lastPosition.Row;
+          token.EndPosition.Column:=currPosition.Column;
+          token.EndPosition.Row:=currPosition.Row;
+
+          fTokenList.Add(token);
+
+          lastPosition.Column:=currPosition.Column+1;
+          lastPosition.Row:=currPosition.Row;
+          value:='';
+        end;
+      end;
+    end;
+    Inc(currPosition.Column);
+  end;
+  fStatus:=tsFinished;
+end;
+
+end.

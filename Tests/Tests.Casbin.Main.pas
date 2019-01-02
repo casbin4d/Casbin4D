393cf592acb89d163f4dbd882a2498c8a2b5e4b2
John Kouraklis <j_kour@hotmail.com>
2019-01-02 03:25:08 +0000

    + Tests

--- Tests/Tests.Casbin.Main.pas
+++ Tests/Tests.Casbin.Main.pas
@@ -44,6 +44,76 @@ type
     [TestCase ('Basic Model.8','..\..\..\Examples\Default\basic_model.conf#'+
                             '..\..\..\Examples\Default\basic_policy.csv#'+
                             'bob,data2,write#true', '#')]
+
+    // From enforce_test.go - TestKeytMatchModelInMemory
+    [TestCase ('KeyMatch.Allow.1','..\..\..\Examples\Tests\keymatch_model_Allow.conf#'+
+                            '..\..\..\Examples\Tests\keymatch_policy.csv#'+
+                            'alice,/alice_data/resource1,GET#true', '#')]
+    [TestCase ('KeyMatch.Allow.2','..\..\..\Examples\Tests\keymatch_model_Allow.conf#'+
+                            '..\..\..\Examples\Tests\keymatch_policy.csv#'+
+                            'alice,/alice_data/resource1,POST#true', '#')]
+    [TestCase ('KeyMatch.Allow.3','..\..\..\Examples\Tests\keymatch_model_Allow.conf#'+
+                            '..\..\..\Examples\Tests\keymatch_policy.csv#'+
+                            'alice,/alice_data/resource2,GET#true', '#')]
+    [TestCase ('KeyMatch.Allow.4','..\..\..\Examples\Tests\keymatch_model_Allow.conf#'+
+                            '..\..\..\Examples\Tests\keymatch_policy.csv#'+
+                            'alice,/alice_data/resource2,POST#false', '#')]
+
+    [TestCase ('KeyMatch.Allow.5','..\..\..\Examples\Tests\keymatch_model_Allow.conf#'+
+                            '..\..\..\Examples\Tests\keymatch_policy.csv#'+
+                            'alice,/bob_data/resource1,GET#false', '#')]
+    [TestCase ('KeyMatch.Allow.6','..\..\..\Examples\Tests\keymatch_model_Allow.conf#'+
+                            '..\..\..\Examples\Tests\keymatch_policy.csv#'+
+                            'alice,/bob_data/resource1,POST#false', '#')]
+    [TestCase ('KeyMatch.Allow.7','..\..\..\Examples\Tests\keymatch_model_Allow.conf#'+
+                            '..\..\..\Examples\Tests\keymatch_policy.csv#'+
+                            'alice,/bob_data/resource2,GET#false', '#')]
+    [TestCase ('KeyMatch.Allow.8','..\..\..\Examples\Tests\keymatch_model_Allow.conf#'+
+                            '..\..\..\Examples\Tests\keymatch_policy.csv#'+
+                            'alice,/bob_data/resource2,POST#false', '#')]
+
+    [TestCase ('KeyMatch.Allow.9','..\..\..\Examples\Tests\keymatch_model_Allow.conf#'+
+                            '..\..\..\Examples\Tests\keymatch_policy.csv#'+
+                            'bob,/bob_data/resource1,GET#false', '#')]
+    [TestCase ('KeyMatch.Allow.10','..\..\..\Examples\Tests\keymatch_model_Allow.conf#'+
+                            '..\..\..\Examples\Tests\keymatch_policy.csv#'+
+                            'bob,/bob_data/resource1,POST#true', '#')]
+    [TestCase ('KeyMatch.Allow.11','..\..\..\Examples\Tests\keymatch_model_Allow.conf#'+
+                            '..\..\..\Examples\Tests\keymatch_policy.csv#'+
+                            'bob,/bob_data/resource2,GET#false', '#')]
+    [TestCase ('KeyMatch.Allow.12','..\..\..\Examples\Tests\keymatch_model_Allow.conf#'+
+                            '..\..\..\Examples\Tests\keymatch_policy.csv#'+
+                            'bob,/bob_data/resource2,POST#true', '#')]
+
+    [TestCase ('KeyMatch.Allow.13','..\..\..\Examples\Tests\keymatch_model_Allow.conf#'+
+                            '..\..\..\Examples\Tests\keymatch_policy.csv#'+
+                            'bob,/alice_data/resource1,GET#false', '#')]
+    [TestCase ('KeyMatch.Allow.14','..\..\..\Examples\Tests\keymatch_model_Allow.conf#'+
+                            '..\..\..\Examples\Tests\keymatch_policy.csv#'+
+                            'bob,/alice_data/resource1,POST#false', '#')]
+    [TestCase ('KeyMatch.Allow.15','..\..\..\Examples\Tests\keymatch_model_Allow.conf#'+
+                            '..\..\..\Examples\Tests\keymatch_policy.csv#'+
+                            'bob,/alice_data/resource2,GET#true', '#')]
+    [TestCase ('KeyMatch.Allow.16','..\..\..\Examples\Tests\keymatch_model_Allow.conf#'+
+                            '..\..\..\Examples\Tests\keymatch_policy.csv#'+
+                            'bob,/alice_data/resource2,POST#false', '#')]
+
+    [TestCase ('KeyMatch.Allow.17','..\..\..\Examples\Tests\keymatch_model_Allow.conf#'+
+                            '..\..\..\Examples\Tests\keymatch_policy.csv#'+
+                            'cathy,/cathy_data,GET#true', '#')]
+    [TestCase ('KeyMatch.Allow.18','..\..\..\Examples\Tests\keymatch_model_Allow.conf#'+
+                            '..\..\..\Examples\Tests\keymatch_policy.csv#'+
+                            'cathy,/cathy_data,POST#true', '#')]
+    [TestCase ('KeyMatch.Allow.19','..\..\..\Examples\Tests\keymatch_model_Allow.conf#'+
+                            '..\..\..\Examples\Tests\keymatch_policy.csv#'+
+                            'cathy,/cathy_data,DELETE#false', '#')]
+
+    // From enforce_test.go - TestKeyMatchModelInMemoryDeny
+    // In the original GO test, it reports the expected value should be true
+    // but it must be wrong????? This returns false
+    [TestCase ('KeyMatchDeny.1','..\..\..\Examples\Tests\keymatch_model_Deny.conf#'+
+                            '..\..\..\Examples\Tests\keymatch_policy.csv#'+
+                            'alice,/alice_data/resource2,POST#false', '#')]
     procedure testEnforce(const aModelFile, aPolicyFile, aEnforceParams: string;
         const aResult: boolean);
   end;

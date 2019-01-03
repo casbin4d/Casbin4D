f572250b09390abd4cbf850c3ed6231c049b5894
John Kouraklis <j_kour@hotmail.com>
2019-01-03 00:13:28 +0000

    + License note in units

--- SourceCode/Common/Loggers/Casbin.Core.Logger.Types.pas
+++ SourceCode/Common/Loggers/Casbin.Core.Logger.Types.pas
@@ -1,4 +1,3 @@
-unit Casbin.Core.Logger.Types;
 // Copyright 2018 The Casbin Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +11,7 @@ unit Casbin.Core.Logger.Types;
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+unit Casbin.Core.Logger.Types;
 
 interface
 
